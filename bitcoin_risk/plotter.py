d6f669c2b62bb776cbc948592b40dc252cb20ac4
roman-ellerbrock <romanellerbrock@gmail.com>
2024-11-23 16:16:05 +0100

    feat: bitcoin log risk metric

---
+++ bitcoin_risk/plotter.py
@@ -0,0 +1,92 @@
+import plotly.graph_objects as go
+import plotly.express as px
+
+def bitcoin_plot(btc):
+
+    def add_colordropdown(fig):
+        colorscales = ['Plotly3', 'Viridis', 'Rainbow', 'Jet']
+        # Add dropdown menu for colorscales
+        fig.update_layout(
+            updatemenus=[
+                dict(
+                    buttons=[
+                        dict(
+                            label=scale,
+                            method="relayout",
+                            args=[{"coloraxis.colorscale": scale}] 
+                        )
+                        for scale in colorscales
+                    ],
+                    direction="down",
+                    showactive=True,
+                )
+            ]
+        )
+        return fig
+
+    def add_line(fig, x, y, color, name):
+        fig.add_trace(
+            go.Scatter(
+                x=x,
+                y=y,
+                mode='lines',
+                name=name,
+                line=dict(color=color),
+                yaxis='y',
+                showlegend=False
+            )
+        )
+        return fig
+
+    # Create the scatter plot for the 'open' prices colored by 'risk'
+    fig = px.scatter(
+        btc,
+        x='date',
+        y='usd',
+        color='risk',  # Color the scatter points by the 'risk' column
+        title='Historical Bitcoin Price and Logarithmic Regression',
+        color_continuous_scale='Turbo'
+    )
+
+    future = btc[btc['date'] > '2024-11-01']
+    add_line(fig, future['date'], future['top'], 'rgba(200, 000, 000, 0.5)', 'top')
+    add_line(fig, future['date'], future['undervalued'], 'rgba(0, 000, 200, 0.5)', 'bottom')
+
+    # Convert the figure to a Plotly Graph Object figure to add a second axis
+    fig = go.Figure(fig)
+
+    # Add a second trace for the 'risk' values
+    fig.add_trace(
+        go.Scatter(
+            x=btc['date'],
+            y=btc['risk'],
+            mode='lines',
+            name='Risk',
+            # line=dict(color='grey'),
+            line=dict(color='rgba(50, 50, 50, 0.3)'),  # Red color with transparency
+            yaxis='y2',
+            showlegend=False,
+        )
+    )
+    fig.update_layout(coloraxis=dict(cmin=-0.1,  cmax=1))
+
+    # Update the layout to include a second y-axis
+    fig.update_layout(
+        yaxis=dict(
+            title='Price / $',
+            type='log'  # Log scale for the open price
+        ),
+        yaxis2=dict(
+            title='Risk',
+            overlaying='y',  # Overlay on the same plot
+            side='right',    # Place it on the right
+            type='linear',    # Make the second y-axis linear
+            showgrid=False
+        ),
+        hovermode='x unified'
+    )
+
+    add_colordropdown(fig)
+
+    fig.write_html('btc.html')
+    return fig
