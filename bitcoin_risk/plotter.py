04ce7968fbae253136c5ac61703d63e3d854cb87
roman-ellerbrock <romanellerbrock@gmail.com>
2024-11-25 00:48:09 +0100

    feat: added time based risk

--- bitcoin_risk/plotter.py
+++ bitcoin_risk/plotter.py
@@ -129,3 +129,81 @@ def bitcoin_risk(btc):
                 layer="below", line_width=0
             )])
     return fig
+
+def bitcoin_plot_time_risk(btc):
+
+    def add_colordropdown(fig):
+        colorscales = ['Plotly3', 'Viridis', 'Rainbow', 'Jet']
+        # Add dropdown menu for colorscales
+        fig.update_layout(
+            updatemenus=[
+                dict(
+                    buttons=[
+                        dict(
+                            label=scale,
+                            method="relayout",
+                            args=[{"coloraxis.colorscale": scale}] 
+                        )
+                        for scale in colorscales
+                    ],
+                    direction="down",
+                    showactive=True,
+                )
+            ]
+        )
+        return fig
+
+    def add_line(fig, x, y, color, name):
+        fig.add_trace(
+            go.Scatter(
+                x=x,
+                y=y,
+                mode='lines',
+                name=name,
+                line=dict(color=color, width=1),
+                yaxis='y',
+                showlegend=False
+            )
+        )
+        return fig
+
+    # Create the scatter plot for the 'open' prices colored by 'risk'
+    fig = px.scatter(
+        btc,
+        x='date',
+        y='usd',
+        color='risk',  # Color the scatter points by the 'risk' column
+        title='Bitcoin price colored by time-risk',
+        color_continuous_scale='Turbo'
+    )
+
+    # future = btc[btc['date'] > '2024-11-01']
+    future = btc
+    # add_line(fig, future['date'], future['fit'], 'rgba(000, 200, 000, 0.4)', 'top')
+    add_line(fig, future['date'], future['top'], 'rgba(200, 000, 000, 0.4)', 'top')
+    add_line(fig, future['date'], future['undervalued'], 'rgba(0, 000, 200, 0.4)', 'bottom')
+
+    # Convert the figure to a Plotly Graph Object figure to add a second axis
+    fig = go.Figure(fig)
+
+    # fig.update_layout(coloraxis=dict(cmin=-0.1,  cmax=1))
+
+    # Update the layout to include a second y-axis
+    fig.update_layout(
+        yaxis=dict(
+            title='Price / $',
+            type='log'  # Log scale for the open price
+        ),
+        yaxis2=dict(
+            title='Risk',
+            overlaying='y',  # Overlay on the same plot
+            side='right',    # Place it on the right
+            type='linear',    # Make the second y-axis linear
+            showgrid=False
+        ),
+        hovermode='x unified'
+    )
+
+    add_colordropdown(fig)
+
+    return fig
