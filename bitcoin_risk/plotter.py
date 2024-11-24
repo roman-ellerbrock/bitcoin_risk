92b8f375b8b76f8926214d060829ffffdd9cf27d
roman-ellerbrock <romanellerbrock@gmail.com>
2024-11-24 15:59:40 +0100

    Added landing page and risk html.

--- bitcoin_risk/plotter.py
+++ bitcoin_risk/plotter.py
@@ -58,18 +58,19 @@ def bitcoin_plot(btc):
     fig = go.Figure(fig)
 
     # Add a second trace for the 'risk' values
-    fig.add_trace(
-        go.Scatter(
-            x=btc['date'],
-            y=btc['risk'],
-            mode='lines',
-            name='Risk',
-            # line=dict(color='grey'),
-            line=dict(color='rgba(50, 50, 50, 0.3)'),  # Red color with transparency
-            yaxis='y2',
-            showlegend=False,
-        )
-    )
+    # fig.add_trace(
+    #     go.Scatter(
+    #         x=btc['date'],
+    #         y=btc['risk'],
+    #         mode='lines',
+    #         name='Risk',
+    #         # line=dict(color='grey'),
+    #         line=dict(color='rgba(50, 50, 50, 0.3)'),  # Red color with transparency
+    #         yaxis='y2',
+    #         showlegend=False,
+    #     )
+    # )
+
     fig.update_layout(coloraxis=dict(cmin=-0.1,  cmax=1))
 
     # Update the layout to include a second y-axis
@@ -90,4 +91,41 @@ def bitcoin_plot(btc):
 
     add_colordropdown(fig)
 
+    return fig
+
+def bitcoin_risk(btc):
+    fig = px.line(btc, x='date', y=['risk'],
+                  title='Historical Bitcoin Price and Logarithmic Regression',
+                  labels={'date': 'Date', 'open-price': 'Open Price', 
+                          'fit': 'Fit', 
+                          'undervalued': 'Undervalued',
+                          'overvalued': 'Overvalued',
+                          'top': 'Bubble Top'})
+    fig.update_traces(mode='lines', hovertemplate='%{y}')
+    fig.update_layout(hovermode='x unified',
+            shapes=[
+            # Green (low risk)
+            dict(
+                type="rect",
+                xref="paper", yref="y",
+                x0=0, x1=1, y0=-0.4, y1=0.15,  # Adjust y0 and y1 for the "low risk" range
+                fillcolor="rgba(0, 255, 0, 0.2)",  # Green with transparency
+                layer="below", line_width=0
+            ),
+            # Orange (medium risk)
+            dict(
+                type="rect",
+                xref="paper", yref="y",
+                x0=0, x1=1, y0=0.15, y1=0.7,  # Adjust y0 and y1 for the "medium risk" range
+                fillcolor="rgba(255, 165, 0, 0.2)",  # Orange with transparency
+                layer="below", line_width=0
+            ),
+            # Red (high risk)
+            dict(
+                type="rect",
+                xref="paper", yref="y",
+                x0=0, x1=1, y0=0.7, y1=1.25,  # Adjust y0 and y1 for the "high risk" range
+                fillcolor="rgba(255, 0, 0, 0.2)",  # Red with transparency
+                layer="below", line_width=0
+            )])
     return fig
